26c66395fce945145327957188b8469f9b64b044
majdirabehdev <m.rabeh.majdi@gmail.com>
2020-07-04 20:07:05 +0200

    Add OkHTTP Logging

--- app/src/main/java/fr/dev/majdi/personna/module/MainModule.kt
+++ app/src/main/java/fr/dev/majdi/personna/module/MainModule.kt
@@ -4,11 +4,14 @@ import fr.dev.majdi.personna.Constants.Companion.BASE_URL
 import fr.dev.majdi.personna.network.ApiPersonna
 import fr.dev.majdi.personna.repository.PersonnaRepository
 import fr.dev.majdi.personna.viewmodel.PersonnaViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 /**
  * Created by Majdi RABEH on 28/06/2020.
@@ -21,10 +24,16 @@ val mainModule = module {
 }
 
 fun createWebService(): ApiPersonna {
+
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+    val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
     val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .baseUrl(BASE_URL)
+        .client(client)
         .build()
 
     return retrofit.create(ApiPersonna::class.java)
